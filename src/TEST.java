a76c17a8f191e6ce6ada0f6bb4a0e91a73609868
Jianjun598 <598ai524@gmail.com>
2015-11-19 19:11:53 +0800

    READ

--- src/TEST.java
+++ src/TEST.java
@@ -3,6 +3,8 @@
  */
 public class TEST {
     public static void main(String[] args){
-              System.out.print("TEST");
+
+        System.out.print("TEST");
+        System.out.print("kao");
     }
 }
